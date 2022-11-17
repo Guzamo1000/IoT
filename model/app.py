432af38183cbcfeda3470b5453cd916ba90c1d6e
Nguyễn Công Hậu <82644341+Guzamo1000@users.noreply.github.com>
2022-11-17 14:19:24 +0700

    d

--- model/app.py
+++ model/app.py
@@ -9,7 +9,7 @@ import cv2
 import urllib.request
 import numpy as np
 import json
-from utils.group import group
+from utils.group import *
 web=Blueprint('web',__name__)
 
 # web.permanent_session_lifetime = timedelta(minutes=1)
@@ -20,25 +20,36 @@ def home():
         cur=mysql.get_db().cursor()
         cur.execute("select Id_thungrac,ViTriThungRac from thungrac")
         listvitri=cur.fetchall()
-        cur.execute("select ID_khoangrac,ID_Thungrac,TenNhan,ViTriThungRac,SoLanDo, Sum(KhoiLuong) from (select khoangrac.ID_khoangrac, thungrac.ID_Thungrac, TenNhan, ViTriThungRac ,KhoiLuong,SoLanDo from ractrongkhoang,khoangrac, thungrac where thungrac.ID_thungrac=khoangrac.ID_Thungrac and khoangrac.ID_khoangrac=ractrongkhoang.ID_khoangrac order by thungrac.ID_Thungrac) as T group by TenNhan")
+        cur.execute("select khoangrac.ID_khoangrac, thungrac.ID_Thungrac, TenNhan, ViTriThungRac ,KhoiLuong,SoLanDo from ractrongkhoang,khoangrac, thungrac where thungrac.ID_thungrac=khoangrac.ID_Thungrac and khoangrac.ID_khoangrac=ractrongkhoang.ID_khoangrac and NgayRacVao>NgayDoRac order by thungrac.ID_Thungrac")
         khoangrac=cur.fetchall()
-        lskhuvuc=[]
+        print(f"khoangrac: {khoangrac}")
+        
         dict_vitri=[]
         for id_khoangrac in listvitri:
             vt={}
+            lskhuvuc=[]
             vt["ID_thungrac"]=id_khoangrac[0]
             vt["ViTriThungRac"]=id_khoangrac[1]
+            kt=False
+            dict_1={}
             for i in khoangrac:
                 if i[1]==id_khoangrac[0]:
+                    # print(i[1])
                     dictkhuvuc={}
                     dictkhuvuc["ID_khoangrac"]=i[0]
                     # dictkhuvuc["ID_Thungrac"]=i[1]
                     dictkhuvuc['TenNhan']=i[2]
                     # dictkhuvuc['ViTriThungRac']=i[3]
-                    dictkhuvuc['SoLanDo']=i[4]
-                    dictkhuvuc['KhoiLuong']=i[5]
+                    # dictkhuvuc['SoLanDo']=i[4]
+                    dictkhuvuc['KhoiLuong']=i[4]
                     lskhuvuc.append(dictkhuvuc)
-            vt['Khoangrac']=lskhuvuc
+                    kt=True
+            if kt==True:
+                newkhuvuc=group_get(lskhuvuc)
+                vt['Khoangrac']=newkhuvuc
+                print(lskhuvuc)
+            else:
+                vt['Khoangrac']="None"
             dict_vitri.append(vt)
         # print(lskhuvuc)
         # lskhuvuc=set(lskhuvuc)
@@ -62,40 +73,43 @@ def home():
                 
                 # ls_time['NgayRacVao']=tm
                 ls_time['KhoiLuong']=i[1]
-                ls_time['NhanRac']=i[2]
+                ls_time['TenNhan']=i[2]
                 # ls_time['KhuVuc']=i[3]
                 ngay.append(ls_time)
             else: 
-                ngay=group(ngay)
+                ngay=group_post(ngay)
                 dir_time[valuetime]=ngay
                 ngay=[]
                 # ls_time['NgayRacVao']=tm
                 ls_time['KhoiLuong']=i[1]
-                ls_time['NhanRac']=i[2]
+                ls_time['TenNhan']=i[2]
                 # ls_time['KhuVuc']=i[3]
                 ngay.append(ls_time)
                 valuetime=tm
                 # print(ls_time)
-        ngay=group(ngay)
+        ngay=group_post(ngay)
         dir_time[valuetime]=ngay
         print(dir_time)
         return jsonify({"status":"success","data": dir_time})
 
 
-@web.route('/reset/<id_thungrac>/<id_khoangrac>/<SoLanDo>',methods=['GET'])
-def reset(id_thungrac,id_khoangrac,SoLanDo):
+
+@web.route('/reset/<id_thungrac>/<id_khoangrac>',methods=['GET'])
+def reset(id_thungrac,id_khoangrac):
     """
     reset KhoiLuong=0 and SoLanDo + 1
     """
     if request.method=='GET':
         id_thungrac=id_thungrac
         id_khoangrac=id_khoangrac
-        SoLanDo=int(SoLanDo)+1
         print(SoLanDo)
         print(id_khoangrac)
         print(id_thungrac)
         cur=mysql.get_db().cursor()
-        cur.execute(f"update khoangrac set KhoiLuong=0,SoLanDo='{SoLanDo}' where ID_Thungrac='{id_thungrac}' and ID_khoangrac='{id_khoangrac}'")
+        cur.execute(f"select * from khoangrac")
+        SoLanDo=cur.fetchall()
+        
+        cur.execute(f"update khoangrac set KhoiLuong=0,SoLanDo='{SoLanDo+1}' where ID_Thungrac='{id_thungrac}' and ID_khoangrac='{id_khoangrac}'")
         mysql.get_db().commit()
      
         return jsonify({"Status":"Success"})
--- utils/group.py
+++ utils/group.py
@@ -1,4 +1,4 @@
-def group(ngay):
+def group_post(ngay):
     bin_1={"NhanRac":"box_cardboard_paper","KhoiLuong":0}
     kl_1,kl_2,kl_3,kl_4=0,0,0,0
     bin_2={"NhanRac":"glass_metal_plastic","KhoiLuong":0}
@@ -6,14 +6,38 @@ def group(ngay):
     bin_4={"NhanRac":"other","KhoiLuong":0}
     newngay=[]
     for d in ngay:
-        if d["NhanRac"]=="box_cardboard_paper":
-            # kl_1+=
+        if d["TenNhan"]=="box_cardboard_paper":
+            bin_1['ID_khoangrac']=d['ID_khoangrac']
             bin_1["KhoiLuong"]+=d["KhoiLuong"]
-        elif d["NhanRac"]=="glass_metal_plastic":
+        elif d["TenNhan"]=="glass_metal_plastic":
             bin_2["KhoiLuong"]+=d["KhoiLuong"]
-        elif d["NhanRac"]=="organic":
+        elif d["TenNhan"]=="organic":
             bin_3["KhoiLuong"]+=d["KhoiLuong"]
         else: 
+            bin_4["KhoiLuong"]+=d["KhoiLuong"]
+    newngay.append(bin_1)
+    newngay.append(bin_2)
+    newngay.append(bin_3)
+    newngay.append(bin_4)
+    return newngay
+def group_get(lskhuvuc):
+    bin_1={"NhanRac":"box_cardboard_paper","KhoiLuong":0}
+    bin_2={"NhanRac":"glass_metal_plastic","KhoiLuong":0}
+    bin_3={"NhanRac":"organic","KhoiLuong":0}
+    bin_4={"NhanRac":"other","KhoiLuong":0}
+    newngay=[]
+    for d in lskhuvuc:
+        if d["TenNhan"]=="box_cardboard_paper":
+            bin_1['ID_khoangrac']=d['ID_khoangrac']
+            bin_1["KhoiLuong"]+=d["KhoiLuong"]
+        elif d["TenNhan"]=="glass_metal_plastic":
+            bin_2['ID_khoangrac']=d['ID_khoangrac']
+            bin_2["KhoiLuong"]+=d["KhoiLuong"]
+        elif d["TenNhan"]=="organic":
+            bin_3['ID_khoangrac']=d['ID_khoangrac']
+            bin_3["KhoiLuong"]+=d["KhoiLuong"]
+        else: 
+            bin_1['ID_khoangrac']=d['ID_khoangrac']
             bin_4["KhoiLuong"]+=d["KhoiLuong"]
     newngay.append(bin_1)
     newngay.append(bin_2)
